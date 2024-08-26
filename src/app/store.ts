862861f44f89dd9949cbd8c1d37dbaa4c4ef2944
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-08-26 15:25:44 +0200

    test: set max state changes to infinity

--- src/app/store.ts
+++ src/app/store.ts
@@ -90,6 +90,9 @@ export function setUpStore(preloadedState?: Partial<RootState>) {
   const store = configureStore({
     reducer: persistedReducer,
     preloadedState: initialState,
+    devTools: {
+      maxAge: Infinity,
+    },
     middleware: (getDefaultMiddleware) => {
       return (
         getDefaultMiddleware({
