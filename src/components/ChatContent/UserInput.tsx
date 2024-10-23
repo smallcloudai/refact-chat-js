6a8d21e12abb119bbc7ff5c8f004cff31b3c52de
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-10-23 18:35:40 +0200

    wip(images): refactor to reuse the original component for the component is an array.

--- src/components/ChatContent/UserInput.tsx
+++ src/components/ChatContent/UserInput.tsx
@@ -18,6 +18,106 @@ import {
 } from "../../services/refact";
 import { takeWhile } from "../../utils";
 
+export type UserInputProps = {
+  children: UserMessage["content"];
+  messageIndex: number;
+  // maybe add images argument ?
+  onRetry: (index: number, question: string) => void;
+  // disableRetry?: boolean;
+};
+
+export const UserInput: React.FC<UserInputProps> = ({
+  messageIndex,
+  children,
+  onRetry,
+}) => {
+  // const { retryFromIndex } = useSendChatRequest();
+  const [showTextArea, setShowTextArea] = useState(false);
+  const [isEditButtonVisible, setIsEditButtonVisible] = useState(false);
+  // const ref = React.useRef<HTMLButtonElement>(null);
+
+  const handleSubmit = useCallback(
+    (value: string) => {
+      onRetry(messageIndex, value);
+      setShowTextArea(false);
+    },
+    [messageIndex, onRetry],
+  );
+
+  const handleShowTextArea = useCallback(
+    (value: boolean) => {
+      setShowTextArea(value);
+      if (isEditButtonVisible) {
+        setIsEditButtonVisible(false);
+      }
+    },
+    [isEditButtonVisible],
+  );
+
+  // const lines = children.split("\n"); // won't work if it's an array
+  const elements = process(children);
+  const isString = typeof children === "string";
+  const linesLength = isString ? children.split("\n").length : Infinity;
+
+  return (
+    <Container position="relative" pt="1">
+      {showTextArea ? (
+        <RetryForm
+          onSubmit={handleSubmit}
+          // TODO
+          // value={children}
+          value=""
+          onClose={() => handleShowTextArea(false)}
+        />
+      ) : (
+        <Flex
+          direction="row"
+          // checking for the length of the lines to determine the position of the edit button
+          gap={linesLength <= 2 ? "2" : "1"}
+          // TODO: what is it's a really long sentence or word with out new lines?
+          align={linesLength <= 2 ? "center" : "end"}
+          my="1"
+          onMouseEnter={() => setIsEditButtonVisible(true)}
+          onMouseLeave={() => setIsEditButtonVisible(false)}
+        >
+          <Button
+            // ref={ref}
+            variant="soft"
+            size="4"
+            className={styles.userInput}
+            // TODO: should this work?
+            // onClick={() => handleShowTextArea(true)}
+            asChild
+          >
+            <div>{elements}</div>
+          </Button>
+          <IconButton
+            title="Edit message"
+            variant="soft"
+            size={"2"}
+            onClick={() => handleShowTextArea(true)}
+            style={{
+              opacity: isEditButtonVisible ? 1 : 0,
+              visibility: isEditButtonVisible ? "visible" : "hidden",
+              transition: "opacity 0.15s, visibility 0.15s",
+            }}
+          >
+            <Pencil2Icon width={15} height={15} />
+          </IconButton>
+        </Flex>
+      )}
+    </Container>
+  );
+};
+
+function process(items: UserInputProps["children"]) {
+  if (typeof items !== "string") {
+    return processUserInputArray(items);
+  }
+
+  return processLines(items.split("\n"));
+}
+
 function processLines(
   lines: string[],
   processedLinesMemo: JSX.Element[] = [],
@@ -54,125 +154,6 @@ function processLines(
   return processLines(next, processedLines);
 }
 
-export type UserInputProps = {
-  children: UserMessage["content"];
-  messageIndex: number;
-  onRetry: (index: number, question: string) => void;
-  // disableRetry?: boolean;
-};
-
-export const UserInput: React.FC<UserInputProps> = ({
-  messageIndex,
-  children,
-  onRetry,
-}) => {
-  // const { retryFromIndex } = useSendChatRequest();
-
-  // TODO: handle other types
-  if (typeof children !== "string") {
-    return <RenderUserInputArray>{children}</RenderUserInputArray>;
-  }
-
-  return (
-    <RenderUserInputString messageIndex={messageIndex} onRetry={onRetry}>
-      {children}
-    </RenderUserInputString>
-  );
-};
-
-const RenderUserInputString: React.FC<
-  Omit<UserInputProps, "children"> & { children: string }
-> = ({ messageIndex, children, onRetry }) => {
-  // const { retryFromIndex } = useSendChatRequest();
-  const [showTextArea, setShowTextArea] = useState(false);
-  const [isEditButtonVisible, setIsEditButtonVisible] = useState(false);
-  // const ref = React.useRef<HTMLButtonElement>(null);
-
-  const handleSubmit = useCallback(
-    (value: string) => {
-      onRetry(messageIndex, value);
-      setShowTextArea(false);
-    },
-    [messageIndex, onRetry],
-  );
-
-  const handleShowTextArea = useCallback(
-    (value: boolean) => {
-      setShowTextArea(value);
-      if (isEditButtonVisible) {
-        setIsEditButtonVisible(false);
-      }
-    },
-    [isEditButtonVisible],
-  );
-
-  const lines = children.split("\n");
-  const elements = processLines(lines);
-
-  return (
-    <Container position="relative" pt="1">
-      {showTextArea ? (
-        <RetryForm
-          onSubmit={handleSubmit}
-          value={children}
-          onClose={() => handleShowTextArea(false)}
-        />
-      ) : (
-        <Flex
-          direction="row"
-          // checking for the length of the lines to determine the position of the edit button
-          gap={lines.length <= 2 ? "2" : "1"}
-          align={lines.length <= 2 ? "center" : "end"}
-          my="1"
-          onMouseEnter={() => setIsEditButtonVisible(true)}
-          onMouseLeave={() => setIsEditButtonVisible(false)}
-        >
-          <Button
-            // ref={ref}
-            variant="soft"
-            size="4"
-            className={styles.userInput}
-            // TODO: should this work?
-            // onClick={() => handleShowTextArea(true)}
-            asChild
-          >
-            <div>{elements}</div>
-          </Button>
-          <IconButton
-            title="Edit message"
-            variant="soft"
-            size={"2"}
-            onClick={() => handleShowTextArea(true)}
-            style={{
-              opacity: isEditButtonVisible ? 1 : 0,
-              visibility: isEditButtonVisible ? "visible" : "hidden",
-              transition: "opacity 0.15s, visibility 0.15s",
-            }}
-          >
-            <Pencil2Icon width={15} height={15} />
-          </IconButton>
-        </Flex>
-      )}
-    </Container>
-  );
-};
-
-type UserInputArray = (
-  | UserMessageContentWithImage
-  | ProcessedUserMessageContentWithImages
-)[];
-
-const RenderUserInputArray: React.FC<{ children: UserInputArray }> = ({
-  children,
-}) => {
-  const items = processUserInputArray(children);
-  return (
-    <Container position="relative" pt="1">
-      <Flex direction="column">{items}</Flex>
-    </Container>
-  );
-};
-
 function isUserContentImage(
   item: UserMessageContentWithImage | ProcessedUserMessageContentWithImages,
 ) {
@@ -183,7 +164,10 @@ function isUserContentImage(
 }
 
 function processUserInputArray(
-  items: UserInputArray,
+  items: (
+    | UserMessageContentWithImage
+    | ProcessedUserMessageContentWithImages
+  )[],
   memo: JSX.Element[] = [],
 ) {
   if (items.length === 0) return memo;
@@ -207,20 +191,32 @@ function processUserInputArray(
   const nextTail = tail.slice(imagesInTail.length);
   const images = [head, ...imagesInTail];
   const elem = (
-    <Flex gap="2" wrap="wrap">
+    <Flex gap="2" wrap="wrap" my="2">
       {images.map((image, index) => {
         if ("type" in image && image.type === "image_url") {
           const key = `user-input${memo.length}-${image.type}-${index}`;
           const content = image.image_url.url;
           return (
-            <Avatar key={key} src={content} size="8" fallback={<ImageIcon />} />
+            <Avatar
+              radius="small"
+              key={key}
+              src={content}
+              size="8"
+              fallback={<ImageIcon />}
+            />
           );
         }
         if ("m_type" in image && image.m_type.startsWith("image/")) {
           const key = `user-input${memo.length}-${image.m_type}-${index}`;
           const content = `data:${image.m_type};base64,${image.m_content}`;
           return (
-            <Avatar key={key} src={content} size="8" fallback={<ImageIcon />} />
+            <Avatar
+              radius="small"
+              key={key}
+              src={content}
+              size="8"
+              fallback={<ImageIcon />}
+            />
           );
         }
         return null;
