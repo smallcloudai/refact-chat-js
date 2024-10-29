4aacacf8f85f2086dbe8a53a08219e7e2af8ead0
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-10-29 13:22:03 +0100

    chore(chat content): fix story book

--- src/components/ChatContent/ChatContent.stories.tsx
+++ src/components/ChatContent/ChatContent.stories.tsx
@@ -1,49 +1,147 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { ChatContent } from ".";
+import { Provider } from "react-redux";
+import { setUpStore } from "../../app/store";
+import { Theme } from "../Theme";
+import { AbortControllerProvider } from "../../contexts/AbortControllers";
+import { MarkdownMessage } from "../../__fixtures__/markdown";
+import { ChatMessages } from "../../events";
+import {
+  CHAT_FUNCTIONS_MESSAGES,
+  CHAT_WITH_DIFF_ACTIONS,
+  CHAT_WITH_DIFFS,
+  FROG_CHAT,
+  LARGE_DIFF,
+} from "../../__fixtures__";
+
+const MockedStore: React.FC<{
+  children: React.ReactNode;
+  messages?: ChatMessages;
+}> = ({ children, messages = [] }) => {
+  const store = setUpStore({
+    chat: {
+      streaming: false,
+      prevent_send: false,
+      waiting_for_response: false,
+      tool_use: "quick",
+      send_immediately: false,
+      error: null,
+      cache: {},
+      system_prompt: {},
+      thread: {
+        id: "test",
+        model: "test",
+        messages,
+      },
+    },
+  });
+  return (
+    <Provider store={store}>
+      <Theme>
+        <AbortControllerProvider>{children}</AbortControllerProvider>
+      </Theme>
+    </Provider>
+  );
+};
 
 const meta = {
   title: "Chat Content",
   component: ChatContent,
   args: {},
+  // decorators: [
+  //   (Story) => (
+  //     <Provider store={store}>
+  //       <Theme>
+  //         <AbortControllerProvider>
+  //           <Story />
+  //         </AbortControllerProvider>
+  //       </Theme>
+  //     </Provider>
+  //   ),
+  // ],
 } satisfies Meta<typeof ChatContent>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  decorators: [
+    (Story) => (
+      <MockedStore>
+        <Story />
+      </MockedStore>
+    ),
+  ],
+};
 
 export const WithFunctions: Story = {
   args: {
     ...meta.args,
     // messages: CHAT_FUNCTIONS_MESSAGES,
   },
+  decorators: [
+    (Story) => (
+      <MockedStore messages={CHAT_FUNCTIONS_MESSAGES}>
+        <Story />
+      </MockedStore>
+    ),
+  ],
 };
 
 export const Notes: Story = {
   args: {
     // messages: FROG_CHAT.messages,
   },
+  decorators: [
+    (Story) => (
+      <MockedStore messages={FROG_CHAT.messages}>
+        <Story />
+      </MockedStore>
+    ),
+  ],
 };
 
 export const WithDiffs: Story = {
   args: {
     // messages: CHAT_WITH_DIFFS,
   },
+  decorators: [
+    (Story) => (
+      <MockedStore messages={CHAT_WITH_DIFFS}>
+        <Story />
+      </MockedStore>
+    ),
+  ],
 };
 
-export const WithDiffActions = {
+export const WithDiffActions: Story = {
   args: {
     // messages: CHAT_WITH_DIFF_ACTIONS.messages,
     // getDiffByIndex: (key: string) => CHAT_WITH_DIFF_ACTIONS.applied_diffs[key],
   },
+  decorators: [
+    (Story) => (
+      <MockedStore messages={CHAT_WITH_DIFF_ACTIONS.messages}>
+        <Story />
+      </MockedStore>
+    ),
+  ],
 };
 
-export const LargeDiff = {
+export const LargeDiff: Story = {
   args: {
     // messages: LARGE_DIFF.messages,
     // getDiffByIndex: (key: string) => LARGE_DIFF.applied_diffs[key],
   },
+  decorators: [
+    (Story) => (
+      <MockedStore messages={LARGE_DIFF.messages}>
+        <Story />
+      </MockedStore>
+    ),
+  ],
 };
 
 export const Empty: Story = {
@@ -53,9 +151,9 @@ export const Empty: Story = {
   decorators: [
     // TODO: use redux store
     (Story) => (
-      // <ConfigProvider config={{ host: "ide" }}>
-      <Story />
-      // </ConfigProvider>
+      <MockedStore messages={[]}>
+        <Story />
+      </MockedStore>
     ),
   ],
 };
@@ -65,4 +163,15 @@ export const AssistantMarkdown: Story = {
     ...meta.args,
     // messages: [["assistant", MarkdownTest]],
   },
+  decorators: [
+    (Story) => {
+      return (
+        <MockedStore
+          messages={[{ role: "assistant", content: MarkdownMessage }]}
+        >
+          <Story />
+        </MockedStore>
+      );
+    },
+  ],
 };
