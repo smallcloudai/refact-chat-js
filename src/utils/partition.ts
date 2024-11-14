62be1d089575dc0220851a95498c8d4d8ebd7a16
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-11-14 11:37:59 +0100

    refactor(partition): add better type names

--- src/utils/partition.ts
+++ src/utils/partition.ts
@@ -1,28 +1,28 @@
-function iter<T, A extends T, B extends T = A>(
+function iter<T, Left extends T = T, Right extends T = T>(
   array: T[],
   predicate: (a: T) => boolean,
-  processed: (A[] | B[])[] = [[], []],
-): (A[] | B[])[] {
+  processed: (Left[] | Right[])[] = [[], []],
+): (Left[] | Right[])[] {
   if (array.length === 0) return processed;
   const [head, ...tail] = array;
   const [left, right] = processed;
 
   if (predicate(head)) {
     // const group = [...lastArray, head] as A[];
-    const nextRight = [...right, head as B] as B[];
+    const nextRight = [...right, head] as Right[];
 
-    const next: (A[] | B[])[] = [left, nextRight];
+    const next: (Left[] | Right[])[] = [left, nextRight];
 
-    return iter<T, A, B>(tail, predicate, next);
+    return iter<T, Left, Right>(tail, predicate, next);
   }
 
-  const nextLeft = [...left, head] as A[];
-  return iter<T, A, B>(tail, predicate, [nextLeft, right]);
+  const nextLeft = [...left, head] as Left[];
+  return iter<T, Left, Right>(tail, predicate, [nextLeft, right]);
 }
 
-export function partition<T, A extends T = T, B extends T = A>(
+export function partition<T, Left extends T = T, Right extends T = T>(
   array: T[],
   condition: (a: T) => boolean,
-): (A[] | B[])[] {
-  return iter<T, A, B>(array, condition);
+): (Left[] | Right[])[] {
+  return iter<T, Left, Right>(array, condition);
 }
