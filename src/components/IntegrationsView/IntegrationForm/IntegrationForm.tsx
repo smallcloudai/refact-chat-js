1e88d86d1cfced77ab954a10d471fc799dc631d5
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-11-29 19:24:54 +0100

    chore(configuration animation): remove old attempt at animating.

--- src/components/IntegrationsView/IntegrationForm/IntegrationForm.tsx
+++ src/components/IntegrationsView/IntegrationForm/IntegrationForm.tsx
@@ -54,8 +54,7 @@ export const IntegrationForm: FC<IntegrationFormProps> = ({
   onValues,
   setAvailabilityValues,
 }) => {
-  const { integration, cachedValues } =
-    useGetIntegrationDataByPathQuery(integrationPath);
+  const { integration } = useGetIntegrationDataByPathQuery(integrationPath);
 
   // const [availabilityValues, setAvailabilityValues] = useState<
   //   Record<string, boolean>
@@ -98,12 +97,10 @@ export const IntegrationForm: FC<IntegrationFormProps> = ({
       field,
       values,
       fieldKey,
-      changed,
     }: {
       fieldKey: string;
       values: Integration["integr_values"];
       field: IntegrationField<NonNullable<IntegrationPrimitive>>;
-      changed: boolean;
     }) => {
       const commonProps = {
         id: fieldKey,
@@ -114,7 +111,6 @@ export const IntegrationForm: FC<IntegrationFormProps> = ({
             ? Number(field.f_default)
             : field.f_default?.toString(), // Otherwise, use the default value from the schema
         placeholder: field.f_placeholder?.toString(),
-        changed,
       };
 
       const maybeSmartlinks = field.smartlinks;
@@ -189,14 +185,11 @@ export const IntegrationForm: FC<IntegrationFormProps> = ({
           )}
         {Object.keys(integration.data.integr_schema.fields).map((fieldKey) => {
           if (integration.data) {
-            const cachedValue = cachedValues && cachedValues[fieldKey];
-            const currentValue = integration.data.integr_values[fieldKey];
             // TBD: does this need all the fields ?
             return renderField({
               fieldKey: fieldKey,
               values: integration.data.integr_values,
               field: integration.data.integr_schema.fields[fieldKey],
-              changed: cachedValue !== currentValue,
             });
           }
         })}
