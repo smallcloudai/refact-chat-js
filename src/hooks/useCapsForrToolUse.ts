f75622051bea96a1cc2a3c4134bd6cb452d6346c
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-12-13 18:44:04 +0100

    chore: remove unused code.

--- src/hooks/useCapsForrToolUse.ts
+++ src/hooks/useCapsForrToolUse.ts
@@ -10,7 +10,7 @@ import {
 
 import { getSelectedChatModel, setChatModel } from "../features/Chat";
 
-// TODO: some shared logic for changing the tool use mode.
+// TODO: hard coded for now.
 const PAID_AGENT_LIST = ["gpt-4o", "claude-3-5-sonnet"];
 
 export function useCapsForToolUse() {
@@ -34,23 +34,6 @@ export function useCapsForToolUse() {
     },
     [caps.data?.code_chat_default_model, dispatch],
   );
-
-  // TODO: causes a loop, handle changing the model on an old chat, this works with a new one, but isn't mounted on an existing chat.
-  //   useEffect(() => {
-  //     if (
-  //       user.data?.inference !== "PRO" &&
-  //       usage.aboveUsageLimit &&
-  //       AGENT_PAY_LIST.includes(currentModel)
-  //     ) {
-  //       setCapModel(caps.data?.code_chat_default_model ?? "");
-  //     }
-  //   }, [
-  //     caps.data?.code_chat_default_model,
-  //     currentModel,
-  //     setCapModel,
-  //     usage.aboveUsageLimit,
-  //     user.data?.inference,
-  //   ]);
 
   const usableModels = useMemo(() => {
     const models = caps.data?.code_chat_models ?? {};
@@ -82,46 +65,3 @@ export function useCapsForToolUse() {
     loading: caps.isFetching || caps.isLoading,
   };
 }
-
-// Shared logic for setting the mode.
-// const AGENT_ALLOW_LIST = ["gpt-4o", "claude-3-5-sonnet"];
-// function modelForMode(
-//   model: string,
-//   caps: ChatFormProps["caps"],
-//   toolUse?: ToolUse,
-// ) {
-//   if (toolUse !== "agent") return model;
-//   // check if paid then they can use any
-
-//   if (AGENT_ALLOW_LIST.includes(model)) return model;
-
-//   const available = Object.keys(caps.available_caps);
-
-//   const hasModels = AGENT_ALLOW_LIST.find((agent) => available.includes(agent));
-//   if (hasModels) return hasModels;
-
-//   return model || caps.default_cap;
-// }
-
-// function modelSupportsAgent(
-//   model: string,
-//   caps: Record<string, CodeChatModel>,
-// ) {
-//   // return AGENT_ALLOW_LIST.includes(model);
-//   if (!(model in caps)) return false;
-//   return caps[model].supports_agent;
-// }
-
-// function capOptionsForMode(
-//   caps: Record<string, CodeChatModel>,
-//   toolUse?: string,
-// ) {
-//   if (toolUse !== "agent") return caps;
-//   const agentEntries = Object.entries(caps).filter(
-//     ([_key, value]) => value.supports_agent,
-//   );
-
-//   if (agentEntries.length === 0) return caps;
-
-//   return Object.fromEntries(agentEntries);
-// }
