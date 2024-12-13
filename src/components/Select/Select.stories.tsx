bdc771cb3ffb855e9a2937ad186e474ba54a34f7
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-12-13 15:51:37 +0100

    ui(select): allow options to be an array of option objects.

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,9 +1,19 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Select } from ".";
+import { Theme, Container } from "@radix-ui/themes";
 
 const meta = {
   title: "Select",
   component: Select,
+  decorators: [
+    (Story) => (
+      <Theme>
+        <Container>
+          <Story />
+        </Container>
+      </Theme>
+    ),
+  ],
 } satisfies Meta<typeof Select>;
 
 export default meta;
@@ -13,6 +23,19 @@ const long = "long".repeat(30);
 export const Default: StoryObj<typeof Select> = {
   args: {
     options: ["apple", "banana", "orange", long],
+    onChange: () => ({}),
+    defaultValue: "apple",
+  },
+};
+
+export const OptionObject: StoryObj<typeof Select> = {
+  args: {
+    options: [
+      { value: "apple" },
+      { value: "banana", disabled: true },
+      { value: "orange" },
+      { value: long },
+    ],
     onChange: () => ({}),
     defaultValue: "apple",
   },
