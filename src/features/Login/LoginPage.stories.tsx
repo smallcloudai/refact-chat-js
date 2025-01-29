e62997a70d4f3bd34f8b822d6f3755d8db3500f0
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-01-29 13:26:44 +0100

    wip(login): add basic ui for login page.

---
+++ src/features/Login/LoginPage.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { LoginPage } from "./LoginPage";
+import { Container, Flex } from "@radix-ui/themes";
+
+import { Provider } from "react-redux";
+import { setUpStore } from "../../app/store";
+import { Theme } from "../../components/Theme";
+// import { CloudLogin } from "../../components/CloudLogin";
+
+const App = () => {
+  const store = setUpStore({
+    config: {
+      apiKey: null,
+      host: "web",
+      lspPort: 8001,
+      addressURL: "Refact",
+      themeProps: { appearance: "dark", accentColor: "gray" },
+    },
+    tour: {
+      type: "finished",
+    },
+  });
+  return (
+    <Provider store={store}>
+      <Theme>
+        <Container p="4">
+          <Flex direction="column" gap="8">
+            {/* <CloudLogin goBack={() => ({})} /> */}
+            <LoginPage />
+          </Flex>
+        </Container>
+      </Theme>
+    </Provider>
+  );
+};
+
+const meta: Meta<typeof App> = {
+  title: "Login",
+  component: App,
+} satisfies Meta<typeof LoginPage>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
---
+++ src/features/Login/LoginPage.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import {
+  Flex,
+  Box,
+  Button,
+  Text,
+  Separator,
+  TextField,
+  Container,
+} from "@radix-ui/themes";
+import { Accordion } from "../../components/Accordion";
+
+export const LoginPage: React.FC = () => {
+  return (
+    <Container>
+      {/* <Box>
+        <Flex
+          direction="column"
+          gap="2"
+          maxWidth="540px"
+          m="8px"
+          style={{ alignSelf: "center" }}
+        >
+          <Button>Create Account</Button>
+        </Flex>
+      </Box> */}
+      <Accordion.Root type="multiple" defaultValue={["cloud"]}>
+        <Accordion.Item value="cloud">
+          <Accordion.Trigger>Refact Cloud</Accordion.Trigger>
+          <Accordion.Content>
+            <Box>
+              <Text size="2">
+                <ul>
+                  <li>
+                    Chat with your codebase powered by top models (e.g. Claude
+                    3.5 Sonnet & GPT-4o with 32k context).
+                  </li>
+                  <li>Unlimited Code Completions (powered by Qwen2.5).</li>
+                  <li>Codebase-aware vector database (RAG).</li>
+                  <li>
+                    Agentic features: browser use, database connect, debugger,
+                    shell commands, etc.
+                  </li>
+                </ul>
+              </Text>
+            </Box>
+            <Separator size="4" my="4" />
+            <Flex direction="column" gap="3">
+              <Button>Continue with Google</Button>
+              <Button>Continue with GitHub</Button>
+              <Flex justify="center">
+                <Text>or</Text>
+              </Flex>
+              <Flex asChild direction="column" gap="3">
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    console.log("submit clicked");
+                    console.log(event);
+                  }}
+                >
+                  <TextField.Root placeholder="Email Address" type="email" />
+                  <Button type="submit">Send magic link</Button>
+                  <Text size="1">
+                    We will send you a one-time login link by email
+                  </Text>
+                </form>
+              </Flex>
+            </Flex>
+          </Accordion.Content>
+        </Accordion.Item>
+        <Accordion.Item value="private">
+          <Accordion.Trigger>Private Server</Accordion.Trigger>
+          <Accordion.Content>
+            <Box>
+              <Text size="2">
+                <ul>
+                  <li>
+                    User your own refact server (Enterprise or self-hosted).
+                  </li>
+                  <li>Fine-tune code completions to your codebase</li>
+                  <li>Keep all code and data under your control.</li>
+                </ul>
+              </Text>
+            </Box>
+          </Accordion.Content>
+        </Accordion.Item>
+        <Accordion.Item value="byok">
+          <Accordion.Trigger>Bring your own key</Accordion.Trigger>
+          <Accordion.Content>
+            <Box>
+              <Text size="2">
+                <ul>
+                  <li>Connect to any OpenAI or Huggingface style server.</li>
+                  <li>
+                    Separate endpoints and keys for chat, completion, and
+                    embedding.
+                  </li>
+                </ul>
+              </Text>
+            </Box>
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
+    </Container>
+  );
+};
+// Accordion
