f9eb8ae5426f49011d33ebd4c49bacbdaa656152
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-01-31 14:42:27 +0100

    fix(console.log): remove console log.

--- src/images/GoogleIcon.tsx
+++ src/images/GoogleIcon.tsx
@@ -3,7 +3,6 @@ import React from "react";
 export const GoogleIcon: React.FC<React.SVGAttributes<SVGElement>> = (
   props,
 ) => {
-  console.log({ props });
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
