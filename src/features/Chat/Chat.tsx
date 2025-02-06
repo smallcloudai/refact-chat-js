44d8f77069ec689209a2e481a2aaeb7a421eb67d
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-02-06 17:45:40 +0100

    wip(routing): handle opening chats from cache, history and new chats.

--- src/features/Chat/Chat.tsx
+++ src/features/Chat/Chat.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import type { Config } from "../Config/configSlice";
 import { Chat as ChatComponent } from "../../components/Chat";
-import { useAppSelector } from "../../hooks";
-import { selectMessages } from "./Thread";
-// import { useParams } from "react-router";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import {
+  newChatAction,
+  restoreChat,
+  selectChatFromCacheOrHistory,
+  selectMessages,
+} from "./Thread";
+import { useParams } from "react-router";
 
 export type ChatProps = {
   host: Config["host"];
@@ -18,8 +23,18 @@ export const Chat: React.FC<ChatProps> = ({
   host,
   tabbed,
 }) => {
+  const dispatch = useAppDispatch();
   const messages = useAppSelector(selectMessages);
-  //  const maybeChatId = useParams().chatId;
+  const maybeChatId = useParams().chatId;
+  const cached = useAppSelector(selectChatFromCacheOrHistory(maybeChatId));
+  if (maybeChatId === undefined) {
+    dispatch(newChatAction());
+  } else if (cached) {
+    dispatch(restoreChat(cached));
+  } else {
+    dispatch(newChatAction());
+  }
+
   // if no chat id make a new chat.
   // if chat id check cache for chat id, then check history for chat id, if not found ... maybe make a new one ?
 
@@ -42,11 +57,16 @@ export const Chat: React.FC<ChatProps> = ({
 
   return (
     <ChatComponent
+      // style not used
       style={style}
+      // host not used
       host={host}
+      // tabbed not used
       tabbed={tabbed}
+      // back ... can be a link
       backFromChat={backFromChat}
       unCalledTools={unCalledTools}
+      // not used
       maybeSendToSidebar={maybeSendToSideBar}
     />
   );
